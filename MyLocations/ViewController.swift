b2804398e3d7bf2cd42508e7ea3850faaf145acd
Manu Safarov <manurockyflacko@gmail.com>
2021-05-26 11:52:19 +0300

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Manu Safarov on 26.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
